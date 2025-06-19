b3576b535a452b94c8ccce4a683df9c716405d1a
Josh Krivoshein <jkrivoshein@gmail.com>
2025-06-19 11:51:23 -0700

    Update artist config with latest changes

--- src/lib/artist.config.ts
+++ src/lib/artist.config.ts
@@ -23,11 +23,56 @@ export const artists: Record<string, Artist> = {
       "https://www.youtube.com/watch?v=IsafbBop9Ys",
     ],
   },
+  intranaut: {
+    name: "Intranaut",
+    photoUrl: "/images/other-dj.jpg",
+    artwork: "/images/other-artwork.jpg",
+    about: "Applecross Collective Admin, DJ and Producer. Also one third of the group Meh.",
+    shows: [
+      { date: "2025-06-20", location: "The Happening" },
+      { date: "2025-09-22", location: "Denver, CO" },
+    ],
+    music: [
+      "https://open.spotify.com/track/41vheDeeZGFFfdV6zwulwT?si=aba214f645674672",
+      "https://soundcloud.com/intranautmusic/traveler-chronicles-001",
+      "https://www.youtube.com/watch?v=ViJiHaomqWw",
+    ],
+  },
+  ourfriendgus: {
+    name: "OurFriendGus",
+    photoUrl: "/images/other-dj.jpg",
+    artwork: "/images/other-artwork.jpg",
+    about: "Applecross Collective Admin, DJ and Producer. Also half of the group Pantry Raid.",
+    shows: [
+      { date: "2025-06-20", location: "Vancouver, CA" },
+      { date: "2025-09-22", location: "Denver, CO" },
+    ],
+    music: [
+      "https://open.spotify.com/track/6wFtTMn2fRCGi5cMjsiX3C?si=0128acb86c7843c5",
+      "https://soundcloud.com/ourfriendgus/applecross-2024-performance",
+      "https://bandcamp.com/EmbeddedPlayer/album=2784637789/size=large/bgcol=ffffff/linkcol=0687f5/track=1/transparent=true/",
+    ],
+  },
+  kfish: {
+    name: "Kfish",
+    photoUrl: "/images/other-dj.jpg",
+    artwork: "/images/other-artwork.jpg",
+    about: "A bass music producer and DJ from the PNW. Also one third of Meh.",
+    shows: [
+      { date: "2025-09-10", location: "Boise, ID" },
+      { date: "2025-09-22", location: "Denver, CO" },
+    ],
+    music: [
+      "https://soundcloud.com/kfishbeats/boyfriend_forever_remix",
+      "https://soundcloud.com/kfishbeats/mac-miller-cold-feet-k-fish-remix",
+      "https://open.spotify.com/track/5G6MfowqL6y2uhoffyw1Hj?si=3295cd288bec42e1",
+    ],
+  },
   lodestone: {
     name: "Lodestone",
     photoUrl: "/images/lodestone.jpg",
     artwork: "/images/lodestone-artwork.jpg",
-    about: "A bass music producer in the PNW.",
+    about: "A bass music producer in the PNW. Also one half of the group Pantry Raid.",
     shows: [
       { date: "2025-09-10", location: "Boise, ID" },
       { date: "2025-09-22", location: "Denver, CO" },
@@ -49,8 +94,8 @@ export const artists: Record<string, Artist> = {
     ],
     music: [
       "https://soundcloud.com/djlodestone/pantryraid-12of30-v1-wip",
-      "https://bandcamp.com/EmbeddedPlayer/album=2000651139/size=large/bgcol=ffffff/linkcol=0687f5/track=1/transparent=true/",
       "https://youtu.be/h2bbFzY61Yc",
+      "https://bandcamp.com/EmbeddedPlayer/album=2000651139/size=large/bgcol=ffffff/linkcol=0687f5/track=1/transparent=true/",
     ],
   },
 };
